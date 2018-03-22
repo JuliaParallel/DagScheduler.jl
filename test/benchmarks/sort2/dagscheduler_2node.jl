80dd43d713f0e4ad696a6bc949e3f93a6e7db153
tan <tanmaykm@gmail.com>
2018-03-22 12:22:00 +0530

    use dref_to_fref! instead of dref_to_fref

--- test/benchmarks/sort2/dagscheduler_2node.jl
+++ test/benchmarks/sort2/dagscheduler_2node.jl
@@ -15,7 +15,7 @@ node2 = NodeEnv(6, getipaddr(), [7,8,9])
 runenv = RunEnv(; nodes=[node1,node2])
 
 const L = 10^6
-const dag2 = DagScheduler.dref_to_fref(DagScheduler.persist_chunks!(gen_sort_dag(L, 40, 4, 40)));
+const dag2 = DagScheduler.dref_to_fref!(DagScheduler.persist_chunks!(gen_sort_dag(L, 40, 4, 40)));
 
 result = collect(rundag(runenv, dag2));
 #@time result = rundag(runenv, dag2);
